463bae9d266b4b331a41d37fd2ac85822e89c485
SamDev-7 <hello@saml.is-a.dev>
2022-09-03 16:33:34 -0400

    Update Co-authored-by: kblazer20 <kblazer20@users.noreply.github.com> Co-authored-by: Jack <peterthecoder1234@users.noreply.github.com> Co-authored-by: jehua-tech <jehua-tech@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,12 +1,62 @@
+import os, mongo
 from flask import Flask
-from flask import render_template
+from flask import render_template, session, request, redirect
 
-app = Flask(__name__)
+db = mongo.MongoDB(os.getenv('MONGODB_URL'), 'accounts', 'users')
+
+app = Flask(__name__, static_folder='./static')
+app.secret_key = os.getenv('SESSION_KEY')
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    '''Creating account with username and password'''
+    if not session.get('logged_in'):
+        # Not logged in
+        if request.method == 'GET':
+            return render_template('signup.html')
+        elif request.method == 'POST':
+            # Create account
+            email = request.form('email')
+            password = request.form['password']
+            if db[email]:
+                # ALready signed up
+                return "You are already signed up"
+            else:
+                # No account exists
+                db[email] = {'password': password}
+                return redirect('/dashboard')
+    else:
+        # Logged in
+        return redirect('/dashboard')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():    
+    '''Will either login or not'''
+    #if request.method
+    if not session.get('logged_in'):
+        # Not logged in
+        if request.method == 'GET':
+            return render_template('login.html')
+        elif request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            # Check if username and password are correct
+            if db[email]:
+                if db[email]['password'] == password:
+                    session['logged_in'] = True
+                    return redirect('/dashboard')
+                else:
+                    return "Incorrect email or password"
+            else:
+                return "This email is not signed up"
+    else:
+        return redirect('/dashboard')
 
 if __name__ == "__main__":
     app.run()
-    itofje5rjgipoersjfdpoiersjgdofvoeirsdf
+    
---
+++ mongo.py
@@ -0,0 +1,44 @@
+from typing import Dict
+from pymongo import MongoClient
+
+class MongoDB():
+
+    def __init__(self, connectionString: str, database:str , collection:str) -> Dict:
+        self._client = MongoClient(connectionString)
+        self._db = self._client[database]
+        self._col = self._db[collection]
+
+    def __setitem__(self, key, value):
+        self._col.update_one({'_id': key}, {"$set": {'_id': key, 'value': value}}, upsert=True)
+    
+    def __getitem__(self, key):
+        result = self._col.find_one({'_id': key})
+
+        if not result:
+            raise KeyError(key)
+        else:
+            return result['value']
+
+    def __delitem__(self, key):
+        result = self._col.delete_one({'_id': key})
+
+        if result.deleted_count == 0:
+            raise KeyError(key)
+    
+    def __len__(self):
+        return self._col.count_documents({})
+    
+    def __iter__(self):
+        return (item for item in self._col.find({}))
+
+    def __contains__(self, key):
+        result = self._col.find_one({'_id': key})
+
+        if not result:
+            return False
+        else:
+            return True
+
+    def items(self):
+        return (item['_id'] for item in self._col.find({}))
+        
